44d1488b724529edf026c8379287f6036ed0ca07
MeKess <faridunqosim@gmail.com>
2022-11-25 22:08:11 +0300

    delete custom paddings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -16,25 +16,11 @@ module.exports = {
 				header: 'repeat(auto-fit, minmax(180px, auto))',
 
 				// body
-				body: 'repeat(auto-fit, minmax(500px, auto))',
-
-				// media_src
-				media: 'repeat(auto-fit, minmax(270px, auto))',
+				body: 'repeat(auto-fit, minmax(390px, auto))',
 			},
 
 			boxShadow: {
 				custom: 'rgb(0 0 0 / 10%) 0px 3px 8px',
-			},
-
-			padding: {
-				// paddings for left plan
-				planPaddsL: '32px 220px 32px 25px',
-
-				// paddings for right plan
-				planPaddsR: '32px 210px 32px 25px',
-
-				// paddings media
-				planPaddsMedia: '32px 23px 50px',
 			},
 		},
 	},
